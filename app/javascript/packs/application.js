37d5d3d7d08e92afef8fb90afce4080c7b7ee0ca
madihamallick <mallickmadiha9031@gmail.com>
2024-01-31 17:32:54 +0530

    minor fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../../assets/stylesheets/application.tailwind.css';
+import '../../assets/builds/tailwind.css';
 
 Rails.start()
 Turbolinks.start()
